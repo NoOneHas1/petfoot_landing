b15b30b55e6405de62792a7cab9f6a470b429242
Luis Angel <luligoluisangel32@gmail.com>
2025-06-16 22:26:12 -0500

    primeros avances

---
+++ assets/layouts/nav.php
@@ -0,0 +1,56 @@
+<?php
+// Define la base URL del proyecto
+$base_url = "/petfoot_landing";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php if (isset($pageTitle)): ?>
+        <title><?php echo $pageTitle; ?></title>
+    <?php else: ?>
+        <title>PetFoot</title>
+    <?php endif; ?>
+
+    <!--fuentes-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!--ALL CSS-->
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/nav.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/index.css">
+
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="<?php echo $base_url; ?>/assets/images/logo/logo.png" alt="">
+            </div>
+            <div class="menu">
+                <nav class="links">
+                    <ul>
+                        <li><a href="#" id="inicio">Inicio</a></li>
+                        <li><a href="#" id="nosotros">¿Quienes somos?</a></li>
+                        <li><a href="#" id="obras">¿Qué hacemos?</a></li>
+                    </ul>
+                </nav>
+                <a href="#" class="btn__start" id="start">Empieza ya!</a>
+            </div>
+        </div>
+    </header>
+    <script src="<?php echo $base_url; ?>/assets/js/menu.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include("assets/layouts/nav.php"); ?>
+
+<main>
+
+    <!--INICIO-->
+    <!--Portada de inicio-->
+    <div class="container__cover div__offset">
+        <div class="cover">
+            <section class="text__cover">
+                <h1>CUIDAMOS LO <br> QUE MAS AMAS</h1>
+                <p>Gestiona la salud y bienestar de tus mascotas desde un solo lugar.  Regístrate gratis y accede a historiales médicos, vacunas, cédulas digitales y más.</p>
+                <a href="./pages/contacto.php" class="btn__text-cover btn__text">Iniciar ahora</a>
+            </section>
+            <section class="image__cover">
+                <img src="assets/images/cover/image_pets.jpeg" alt="">
+            </section>
+        </div>
+    </div>
+
+    <!--SERVICIOS-->
+
+    <div class="container__service container__card-primary div__offset">
+        <div class="service card__primary">
+            <div class="text__service text__card-primary">
+                <p>¿QUE HACEMOS?</p>
+                <h1>Cuidamos y protegemos tu mascota</h1>
+            </div>
+
+            <div class="container__card-service container__box-cardPrimary">
+                <div class=" card__service box__card-primary">
+                    <img src="assets/images/icons/dog-solid.svg" alt="">
+                    <p> Agrega y gestiona tus mascotas en un solo lugar</p>
+                </div>
+                <div class=" card__service box__card-primary">
+                    <img src="assets/images/icons/paw-solid.svg" alt="">
+                    <p>Accede al historial completo de salud de tu mascota.</p>
+                </div>
+                <div class=" card__service box__card-primary">
+                    <img src="assets/images/icons/stethoscope-solid.svg" alt="">
+                    <p>Veterinarios autorizados registran diagnósticos y cuidados.</p>
+                </div>
+                <div class=" card__service box__card-primary">
+                    <img src="assets/images/icons/user-doctor-solid.svg" alt="">
+                    <p>Identificador único para cada mascota.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Quienes somos-->
+    <div class="container__about container__card-primary div__offset">
+        <div class="about card__primary">
+            <div class="text__about text__card-primary">
+                <p>¿QUIENES SOMOS?</p>
+                <h1>Unimos tecnología y amor por los animales</h1>
+            </div>
+
+
+
+</main>
